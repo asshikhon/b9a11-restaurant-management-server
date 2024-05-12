513d5a1225d8c0f891849fa9bea13e22beded1c0
asshikhon <asshikhon@gmail.com>
2024-05-13 04:30:37 +0600

    added single id get functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -45,6 +45,13 @@ async function run() {
             const result = await foodCollection.find().toArray();
             res.send(result);
         });
+
+        app.get('/food/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)};
+        const result = await foodCollection.findOne(query);
+        res.send(result);
+        })
 
         app.post('/gallery', async (req, res) => {
             const newGallery = req.body;
